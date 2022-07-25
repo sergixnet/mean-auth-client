bc2d803022ddc83349de6fed0c4023cd6e411e2c
Sergio Peña <sergiop79@gmail.com>
2022-07-25 18:27:28 +0200

    chapter 388

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: [],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  miFormulario: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  login() {
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
+  }
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  miFormulario: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  register(): void {
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
   }
-
 }
