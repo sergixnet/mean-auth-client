7ee06498715fe47e89126fec9d4425c5fe52a056
Sergio Peña <sergiop79@gmail.com>
2022-07-27 07:07:07 +0200

    Chapter 390

---
+++ src/app/auth/interfaces/interfaces.ts
@@ -0,0 +1,7 @@
+export interface AuthResponse {
+  ok: string;
+  uid?: string;
+  name?: string;
+  token?: string;
+  msg?: string;
+}
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +14,19 @@ export class LoginComponent {
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   login() {
     console.log(this.miFormulario.value);
+    const { email, password } = this.miFormulario.value;
 
-    this.router.navigateByUrl('/dashboard');
+    this.authService.login(email, password).subscribe((resp) => {
+      console.log(resp);
+    });
+    // this.router.navigateByUrl('/dashboard');
   }
 }
