124097b14e18e804e4ad7d8270a86c6b62c84430
Sergio Peña <sergiop79@gmail.com>
2022-07-28 07:05:30 +0200

    Chapter 392

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -25,10 +26,10 @@ export class LoginComponent {
     const { email, password } = this.miFormulario.value;
 
     this.authService.login(email, password).subscribe((ok) => {
-      if (ok) {
+      if (ok === true) {
         this.router.navigateByUrl('/dashboard');
       } else {
-        // TODO: mostrar mensaje de error
+        Swal.fire('Error', ok, 'error');
       }
     });
     // this.router.navigateByUrl('/dashboard');
